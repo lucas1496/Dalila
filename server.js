d78fa9307d22d725da85835e76e292cfa8f4b664
Lucas Q <lucas.14.96@gmail.com>
2021-05-27 15:00:48 -0500

    Changed server port to 3001

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ if (process.env.NODE_ENV = "production") {
 }
  */
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
